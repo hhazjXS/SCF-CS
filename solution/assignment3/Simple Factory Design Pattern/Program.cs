eb59d5172889e005f6f0a11397689b5022e1115d
hhazj <2933875655@qq.com>
2023-03-09 20:02:33 +0800

    first commit

---
+++ solution/assignment3/Simple Factory Design Pattern/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Security.Principal;
+
+/**
+    *在简单工厂模式中，有三个主要的角色：
+    *Product（产品类）：需要被创建的类或对象的父类或接口。这个类或接口定义了对象的基本属性和方法。
+    *ConcreteProduct（具体产品类）：实现了Product接口的具体类或对象。它们是工厂方法创建的对象。
+    *Factory（工厂类）：创建具体产品类的类。它是实现了创建对象的代码逻辑的地方。
+ */
+
+namespace SFDP
+{
+    //定义一个返回面积的接口
+    interface IShape
+    {
+        double GetArea();
+    }
+    //下面来定义不同形状类来实现IShape接口
+
+    //长方形
+    class Rectangle : IShape
+    {
+        private double length, width;
+        //构造函数
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        //实现接口
+        public double GetArea()
+        {
+            return length * width;
+        }
+    }
+
+    //正方形
+    class Square : IShape
+    {
+        private double side;
+        public Square(double side)
+        {
+            this.side = side;
+        }
+        //实现接口
+        public double GetArea()
+        {
+            return side * side;
+        }
+    }
+
+    //圆形
+    class Circle : IShape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        //实现接口
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    //三角形
+    class Triangle : IShape
+    {
+        private double a, b, c;
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        //判断是否构成三角形
+        public bool Judge(double a, double b, double c)
+        {
+            return a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (b + c) > a;
+        }
+        //实现接口
+        public double GetArea()
+        {
+            double tem = (a + b + c) / 2;
+            return Math.Sqrt(tem * (tem - a) * (tem - b) * (tem - c));
+        }
+    }
+
+    //下面定义工厂类(创建具体的类)
+    class ShapeFactory
+    {
+        //创建一个随机数
+        private Random rand = new Random();
+        public IShape CreateShape()
+        {
+            switch (rand.Next(4))
+            {
+                case 0:
+                    return new Rectangle(rand.Next(1, 9), rand.Next(1, 9));
+                case 1:
+                    return new Square(rand.Next(1, 9));
+                case 2:
+                    return new Circle(rand.Next(1, 9));
+                case 3://判断是否构成三角形
+                    while (true)
+                    {
+                        double a = rand.Next(1, 9);
+                        double b = rand.Next(1, 9);
+                        double c = rand.Next(1, 9);
+                        Triangle Tri = new Triangle(a, b, c);
+                        if (Tri.Judge(a, b, c))
+                        {
+                            return Tri;
+                        }
+                    }
+                default:
+                    throw new Exception("随机数生成错误，检查随机数的生成！");
+            }
+        }
+    }
+
+    class SumArea
+    {
+        static void Main(string[] args)
+        {
+            //实现
+            double sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                ShapeFactory factory = new ShapeFactory();
+                sum += factory.CreateShape().GetArea();
+            }
+            Console.WriteLine("The total area: " + sum);
+        }
+    }
+}
---
+++ solution/assignment3/class design/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+//C#里面的属性和方法都是Pascal原则，即首字母都大写
+//变量才使用camel原则，即首字母小写，其它首字母大写
+namespace Shapes
+{
+
+    /*
+     *方法一：直接使用抽象类实现
+     */
+
+    // 定义一个抽象类，表示基础形状，包含计算面积的方法
+    /*public abstract class Shape
+    {
+        public abstract double Area { get; }
+        public abstract bool IsValid(); // 子类实现
+    }*/
+
+
+    /*
+     *方法二：利用接口再通过抽象类重写实现
+     */
+
+    // 定义一个接口，表示可计算面积的形状
+    //接口的名字前最好加一个大写的I
+    public interface IShape
+    {
+        double Area { get; } // 只读面积
+        bool IsValid(); // 判断形状是否合法
+    }
+
+    // 定义一个抽象类，表示基础形状，包含计算面积的方法
+    public abstract class Shape : IShape
+    {
+        public abstract double Area { get; }
+        public abstract bool IsValid(); // 子类实现
+    }
+
+    // 长方形类
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double Area
+        {
+            get { return length * width; }
+        }
+
+        public override bool IsValid()
+        {
+            return length > 0 && width > 0;
+        }
+    }
+
+    // 正方形类
+    public class Square : Shape
+    {
+        private double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public override double Area
+        {
+            get { return side * side; }
+        }
+
+        public override bool IsValid()
+        {
+            return side > 0;
+        }
+    }
+
+    // 三角形类
+    public class Triangle : Shape
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                // 使用海伦公式计算三角形面积
+                double p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+
+        public override bool IsValid()
+        {
+            // 判断是否满足三角形的边长条件
+            return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = 0, width = 0;
+            double side = 0;
+            double a = 0, b = 0, c = 0;
+
+            //异常值处理
+            try
+            {
+                Console.Write("请分别输入长方形的长和宽：");
+                length = Convert.ToDouble(Console.ReadLine());
+                width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("请输入正方形的边长：");
+                side = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("请分别输入三角形的三条边长：");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"错误：{e.Message}");
+            }
+            Rectangle rect = new Rectangle(length, width);
+            Square square = new Square(side);
+            Triangle triangle = new Triangle(a, b, c);
+
+            //计算面积
+            if (rect.IsValid())
+            {
+                Console.WriteLine("长方形的面积为：" + rect.Area);
+            }
+            if (square.IsValid())
+            {
+                Console.WriteLine("正方形的面积为：" + square.Area);
+            }
+            if (triangle.IsValid())
+            {
+                Console.WriteLine("三角形的面积为：" + triangle.Area);
+            }
+        }
+    }
+}
